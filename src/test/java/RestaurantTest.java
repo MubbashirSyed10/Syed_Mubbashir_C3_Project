65a1389b579ee3e85942661beed6c46b564927e1
Mubbashir <m10syed@gmail.com>
2021-09-29 13:46:59 +0530

    Part 3: Solution

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -25,7 +25,6 @@ class RestaurantTest {
         restaurant.addToMenu("Sweet corn soup",119);
         restaurant.addToMenu("Vegetable lasagne", 269);
     }
-    Item items = new Item("Sizzling brownie",319);
 
     //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     //-------FOR THE 2 TESTS BELOW, YOU MAY USE THE CONCEPT OF MOCKING, IF YOU RUN INTO ANY TROUBLE
